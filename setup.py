d259cd5e139e95d9f2afe805ff394cc751a1e8d4
777moneymaker <adeeeq77@gmail.com>
2021-01-15 15:22:54 -0800

    version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md") as fh:
 
 setup(
     name='jasper_vh',
-    version='1.0.1',
+    version='1.0.2',
     description="Just a simple virus's hosts predictor",
     long_description=description,
     long_description_content_type='text/markdown',
