changed afree to wish, added mash, fixed merge, sub normalization to ranks

jasper/merge.py
@@ -2,8 +2,9 @@
 """This module manages the final merging of results from each single module."""
 
 import numbers
+import numpy as np
 from itertools import chain
-
+from functools import reduce
 import pandas as pd
 
 
@@ -41,23 +42,30 @@ def main(args):
     Args:
         args (argparse obj): Arguments from right subparser.
     """
-    if len(args.files) != len(args.weights):
-        raise ValueError("Number of files and weights is different.")
-    if sum(args.weights) != 1:
-        raise ValueError("Weights don't sum up to 1.")
+    # if len(args.files) != 4:
+    #     raise ValueError("You didn't specify every result file")
     frames = list(map(pd.read_csv, args.files))
-    frames = multiply_frames_by(frames, args.weights)
+    concat = reduce(lambda left, right: pd.merge(left, right, on=['Virus', 'Host'], how='outer'), frames)
+    concat[concat.columns.to_list()[2:]].apply(pd.to_numeric)
 
-    final = pd.DataFrame(columns=['Virus', 'Host', 'Score'])
-    final['Virus'] = list(chain(*list(map(lambda df: df['Virus'].tolist(), frames))))
-    final['Host'] = list(chain(*list(map(lambda df: df['Host'].tolist(), frames))))
-    final['Score'] = list(chain(*list(map(lambda df: df['Score'].tolist(), frames))))
-    final['Score'] = final['Score'].apply(pd.to_numeric)
 
-    final = final.groupby(["Virus", "Host"]).sum().reset_index()
-    idx = final.groupby(['Virus'])['Score'].idxmax()
-    final = final.loc[idx].sort_values('Score', ascending=False).reset_index(drop=True)
+    concat = concat[[c for c in concat if c.endswith('Rank') or c in ("Virus", "Host")]]
+    concat = concat.reindex(["Virus", "Host", "BlastRank", "CrisprRank", "tRNARank", "WishRank", "MashRank"], axis=1)
+    concat.dropna(axis=1, inplace=True, how='all')
+    concat.fillna({c: np.inf for c in concat.columns.to_list()[2:]}, inplace=True)
 
-    final['Score'] = final['Score'].round(decimals=2)
-    final.to_csv(args.output, index=False)
+    # frames = multiply_frames_by(frames, args.weights)
+    # final = pd.DataFrame(columns=['Virus', 'Host', 'Score'])
+    # final['Virus'] = list(chain(*list(map(lambda df: df['Virus'].tolist(), frames))))
+    # final['Host'] = list(chain(*list(map(lambda df: df['Host'].tolist(), frames))))
+    # final['Score'] = list(chain(*list(map(lambda df: df['Score'].tolist(), frames))))
+    # final['Score'] = final['Score'].apply(pd.to_numeric)
+    #
+    # final = final.groupby(["Virus", "Host"]).sum().reset_index()
+    # idx = final.groupby(['Virus'])['Score'].idxmax()
+    # final = final.loc[idx].sort_values('Score', ascending=False).reset_index(drop=True)
+    #
+    # final['Score'] = final['Score'].round(decimals=2)
+    # final.to_csv(args.output, index=False)
+    print(concat.sort_values(by=concat.columns.to_list()[2:]).reset_index(drop=True))
     print('Saved final results to', args.output)